0c6833fcb926d9393bd3214ebf059c93e5093179
Daniil Goncharov <goncharov10d@gmail.com>
2023-12-27 15:22:01 +0500

    ver.1

---
+++ utils/constants.js
@@ -0,0 +1,138 @@
+const langArr = {
+  "menu-main" : {
+    "ru": "Главное",
+    "en": "Main"
+  },
+  "menu-about" : {
+    "ru": "Кто я",
+    "en": "Who I am"
+  },
+  "menu-projects" : {
+    "ru": "Мои проекты",
+    "en": "My projects"
+  },
+  "menu-contact" : {
+    "ru": "Контакты",
+    "en": "Contact"
+  },
+  "name" : {
+    "ru": "Даниил Гончаров",
+    "en": "Daniil Goncharov"
+  },
+  "job" : {
+    "ru": "Разработчик электронных курсов, методист",
+    "en": "E-learning developer, methodologist"
+  },
+  "about" : {
+    "ru": "Мой опыт",
+    "en": "My experience"
+  },
+  "short-description" : {
+    "ru": "Здравствуйте! Меня зовут Даниил Гончаров, я профессиональный разработчик электронных курсов и методист. С 2016 года занимаюсь созданием интерактивных образовательных материалов. Специализируюсь на проектировании и разработке курсов, охватывающих широкий спектр тем, от технических навыков до мягких навыков и профессионального развития.",
+    "en": "Hello! My name is Daniil Goncharov, and I am a professional e-learning course developer and instructional designer. Since 2016, I have been involved in creating interactive educational materials. I specialize in designing and developing courses that cover a wide range of topics, from technical skills to soft skills and professional development."
+  },
+  "long-description-one" : {
+    "ru": "Методическая работа: Разработал десятки тренингов и сценариев электронных курсов.",
+    "en": "Instructional Work: I have developed dozens of training sessions and scenarios for e-learning courses."
+  },
+  "long-description-two" : {
+    "ru": "Программы для разработки онлайн курсов: Профессионально владею программами ispring, Courcelab, Articulate storyline и Figma. По мимо этого обладаю навыками программирования на JavaScript и разработки сайтов.",
+    "en": "Online Course Development Tools: Proficient in using programs such as iSpring, Courcelab, Articulate Storyline, and Figma for online course development. Additionally, I possess programming skills in JavaScript and website development."
+  },
+  "long-description-three" : {
+    "ru": "Интерактивные элементы: Разрабатываю интерактивные элементы, такие как тесты, квизы и задания, чтобы обеспечить активное участие студентов в процессе обучения.",
+    "en": "Interactive Elements: I design interactive elements, such as tests, quizzes, and assignments, to ensure active student participation in the learning process."
+  },
+  "long-description-four" : {
+    "ru": "Платформы для онлайн обучения: Опыт работы с различными платформами для онлайн обучения, такими как iSpring Learn, Websoft HCM, Bitrix, Moodle и другие. Умею адаптировать контент под различные форматы и стандарты SCORM.",
+    "en": "Online Learning Platforms: Experienced in working with various online learning platforms, including iSpring Learn, Websoft HCM, Bitrix, Moodle, and others. I can adapt content to different formats and SCORM standards."
+  },
+  "long-description-five" : {
+    "ru": "Аналитика и оценка: Знаю как применять инструменты аналитики для оценки эффективности обучения, отслеживания прогресса студентов и внесения корректив в курсы для оптимизации результатов.",
+    "en": "Analytics and Evaluation: Knowledgeable in applying analytical tools to assess the effectiveness of training, track student progress, and make course adjustments to optimize results."
+  },
+  "skills" : {
+    "ru": "Мои навыки",
+    "en": "My skills"
+  },
+  "courses" : {
+    "ru": "Примеры моих курсов",
+    "en": "My courses"
+  },
+  "open-course" : {
+    "ru": "Открыть курс",
+    "en": "Open course"
+  },
+  "courses-1" : {
+    "ru": "Интерактивный курс-тренажер 1C",
+    "en": "Interactive course-simulator 1C"
+  },
+  "courses-info-1" : {
+    "ru": "Пример интерактивного курса-тренажера, обучающего работе в программе 1С",
+    "en": "An example of an interactive training course that teaches how to work in the 1C program"
+  },
+  "courses-2" : {
+    "ru": "Интерактивный курс-тренажер по кассовой программе",
+    "en": "Interactive course-simulator on the cash program"
+  },
+  "courses-info-2" : {
+    "ru": "Пример интерактивного курса-тренажера, обучающего работе в программе 1С",
+    "en": "An example of an interactive training course that teaches how to work in the Frontol cash register program"
+  },
+  "courses-3" : {
+    "ru": "Курс о правилах деловой переписки",
+    "en": "Course on the rules of business correspondence"
+  },
+  "courses-info-3" : {
+    "ru": "Пример интерактивного курса для офисных сотрудников о правилах написания деловых писем",
+    "en": "An example of an interactive course for office workers on the rules for writing business letters"
+  },
+  "courses-4" : {
+    "ru": "Вводный курс по аквариумистике",
+    "en": "Introductory aquarium course"
+  },
+  "courses-info-4" : {
+    "ru": "Пример интерактивного курса, обучающего стажеров аквариумистов",
+    "en": "An example of an interactive course teaching trainee aquarists"
+  },
+  "courses-5" : {
+    "ru": "Курс по продукту",
+    "en": "Product course"
+  },
+  "courses-info-5" : {
+    "ru": "Пример интерактивного курса по продукту - промышленный корм для животных. Аудитория - продавцы зоомагазина",
+    "en": "An example of an interactive product course is Industrial Animal Feed. Audience - pet store salespeople"
+  },
+  "linkedin" : {
+    "ru": "Моя страница в LinkedIn",
+    "en": "My page in LinkedIn"
+  },
+  "telegram" : {
+    "ru": "Мой Telegram",
+    "en": "My Telegram"
+  },
+  "view-website-1" : {
+    "ru": "Посмотреть сайт&nbsp;&rarr;",
+    "en": "View website&nbsp;&rarr;"
+  },
+  "view-website-2" : {
+    "ru": "Посмотреть сайт&nbsp;&rarr;",
+    "en": "View website&nbsp;&rarr;"
+  },
+  "view-website-3" : {
+    "ru": "Посмотреть сайт&nbsp;&rarr;",
+    "en": "View website&nbsp;&rarr;"
+  },
+  "view-website-4" : {
+    "ru": "Посмотреть сайт&nbsp;&rarr;",
+    "en": "View website&nbsp;&rarr;"
+  },
+  "view-website-5" : {
+    "ru": "Посмотреть сайт&nbsp;&rarr;",
+    "en": "View website&nbsp;&rarr;"
+  },
+  "contact" : {
+    "ru": "Контакты",
+    "en": "Contact"
+  },
+};
